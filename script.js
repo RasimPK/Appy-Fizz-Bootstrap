c6bf80d1ecdb39737c0a219be15f8de71127362b
RasimPK <raseempkraseem@gmail.com>
2024-10-03 20:48:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let elem = document.getElementById('rotate');
+window.addEventListener('scroll',() => {
+    let scrollposition = window.scrollY;
+    let rotate = scrollposition *0.35;
+    elem.style.transform = `rotate(${rotate}deg)`;
+})
